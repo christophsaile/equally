e20dff85ac343450d6509e331bed09081e6b057e
Christoph <christoph.saile@googlemail.com>
2024-08-20 22:16:04 +0200

    feat: add basic styling

--- app/first-login/page.tsx
+++ app/first-login/page.tsx
@@ -13,12 +13,40 @@ export default async function FirstLogin() {
   }
 
   return (
-    <form action={updateProfileData} className="mb-8 flex flex-col gap-2">
-      <label htmlFor="first_name">First Name</label>
-      <input id="first_name" name="first_name" type="text" required />
-      <label htmlFor="last_name">Last Name</label>
-      <input id="last_name" name="last_name" type="text" required />
-      <button type="submit" name="button" value="submit">
+    <form
+      action={updateProfileData}
+      className="mb-8 flex flex-col gap-4 rounded border p-4 shadow-md"
+    >
+      <div className="flex flex-col">
+        <label htmlFor="first_name" className="mb-1 font-semibold">
+          First Name
+        </label>
+        <input
+          id="first_name"
+          name="first_name"
+          type="text"
+          required
+          className="rounded border p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="last_name" className="mb-1 font-semibold">
+          Last Name
+        </label>
+        <input
+          id="last_name"
+          name="last_name"
+          type="text"
+          required
+          className="rounded border p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <button
+        type="submit"
+        name="button"
+        value="submit"
+        className="mt-4 rounded bg-blue-500 p-2 text-white hover:bg-blue-600"
+      >
         Save Changes
       </button>
     </form>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,13 +4,45 @@ import { login, signup } from "./actions";
 // TODO - Add status messages for login and signup
 export default function Login() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
+    <form className="mb-8 flex flex-col gap-4 rounded border p-4 shadow-md">
+      <div className="flex flex-col">
+        <label htmlFor="email" className="mb-1 font-semibold">
+          Email:
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          className="rounded border p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password" className="mb-1 font-semibold">
+          Password:
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          className="rounded border p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="flex gap-4">
+        <button
+          formAction={login}
+          className="mt-4 rounded bg-blue-500 p-2 text-white hover:bg-blue-600"
+        >
+          Log in
+        </button>
+        <button
+          formAction={signup}
+          className="mt-4 rounded bg-green-500 p-2 text-white hover:bg-green-600"
+        >
+          Sign up
+        </button>
+      </div>
     </form>
   );
 }
