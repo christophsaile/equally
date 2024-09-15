d4a34f735c6bff0c0b048f80572937adcde0e8b2
Christoph <christoph.saile@googlemail.com>
2024-09-15 19:23:06 +0200

    fix: z-index issues

--- components/form-expense.tsx
+++ components/form-expense.tsx
@@ -97,10 +97,10 @@ export default function FormExpense({
               min="0"
               defaultValue={amount}
             />
-            <div className="pointer-events-none absolute inset-y-0 start-0 z-20 flex items-center ps-4">
+            <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-4">
               <span className="text-gray-500 dark:text-neutral-500">€</span>
             </div>
-            <div className="pointer-events-none absolute inset-y-0 end-0 z-20 flex items-center pe-4">
+            <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-4">
               <span className="text-gray-500 dark:text-neutral-500">EUR</span>
             </div>
           </div>
@@ -120,7 +120,7 @@ export default function FormExpense({
             data-hs-select='{
                 "toggleTag": "<button type=\"button\" aria-expanded=\"false\"></button>",
                 "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-base focus-style dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400",
-                "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700",
+                "dropdownClasses": "mt-2 z-20 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700",
                 "optionClasses": "py-2 px-4 w-full text-base text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800",
                 "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-blue-500 \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>",
                 "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
@@ -129,8 +129,8 @@ export default function FormExpense({
           >
             <option value="1">You paid, split equally</option>
             <option value="2">You are owed the full amount</option>
-            <option value="3">Someone else paid, split equally</option>
-            <option value="4">Someone else is owed the full amount</option>
+            <option value="3">Other person paid, split equally</option>
+            <option value="4">Other person is owed the full amount</option>
           </select>
         </div>
       </div>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export function Navigation({ children }: Props) {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 m-auto grid max-w-lg grid-cols-2 items-center gap-4 px-4 pb-8">
+    <nav className="fixed bottom-0 left-0 right-0 z-10 m-auto grid max-w-lg grid-cols-2 items-center gap-4 px-4 pb-8">
       {children}
     </nav>
   );
