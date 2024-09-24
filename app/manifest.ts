06738ba0157c85d4f1ed6b82c2ed962ff15b342c
Christoph <christoph.saile@googlemail.com>
2024-09-24 18:50:28 +0200

    feat: add favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,18 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#171717",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/icon-mask.png",
+        type: "image/png",
+        sizes: "512x512",
+        purpose: "maskable",
+      },
+      {
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
